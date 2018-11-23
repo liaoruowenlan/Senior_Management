f5f34e1abba67b12a2831a19d1a922434a062aa4
Zcy <576124845@qq.com>
2018-11-24 01:53:45 +0800

    修改左侧siderMenu左侧导航栏组件中的文字。

--- src/components/SiderMenu/SiderMenu.js
+++ src/components/SiderMenu/SiderMenu.js
@@ -82,7 +82,7 @@ export default class SiderMenu extends PureComponent {
         <div className={styles.logo} id="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
-            <h1>Ant Design Pro</h1>
+            <h1> intellif </h1>
           </Link>
         </div>
         <BaseMenu
